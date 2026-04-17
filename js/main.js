16299e19a4a688133958ceb22215de264f70dc4e
hnkholiya <harshilkholiya@gmail.com>
2026-04-17 10:07:27 +0530

    home page ui update and base64 tool added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+// Load saved theme
+if(localStorage.getItem("theme") === "dark"){
+  document.body.classList.add("dark");
+}
+
+toggleBtn.onclick = () => {
+  document.body.classList.toggle("dark");
+
+  if(document.body.classList.contains("dark")){
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
